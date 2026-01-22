13c37be06470936cc835f3da1d9cd9c1959a59fc
Yu Fang <newfarry@126.com>
2026-01-22 20:54:56 +0800

    用flutter_markdown_plus显示回复

--- lib/pages/agent_detail_page.dart
+++ lib/pages/agent_detail_page.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import '../services/agent_service.dart';
 
 class AgentDetailPage extends StatefulWidget {
--- lib/pages/dialog_detail_page.dart
+++ lib/pages/dialog_detail_page.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import '../models/dialog.dart';
 import '../services/chat_service.dart';
 
